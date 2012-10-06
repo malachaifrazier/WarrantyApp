cd9f9910e9197a5c97643675de4f2f25d88a3d6d
malachaifrazier <malachaifrazier@gmail.com>
2012-10-06 01:27:29 -0400

    Devise User Model Generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 MagnetiApp::Application.routes.draw do
   
+  devise_for :users
+
   get "home/index"
 
-   root :to => "home#index"
+  root :to => "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
