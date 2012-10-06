945499306021f4edc67fc90fcb6ebbc1caea59cf
malachaifrazier <malachaifrazier@gmail.com>
2012-10-05 23:28:37 -0400

    New App! #gklst

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MagnetiApp::Application.config.secret_token = '0995bf19c4460f96d0e0aa4429eaf9cc5ba2a9ff336c2dbfa4c9d2246240e66d16260b657cdabb6d536d99ee1a958081caadd1bf4fa4ed74265387d39a86c6b7'
